d18d4d187861ebc339ce9ac3ce8a27c527a01e26
Clement <clement-martins@live.fr>
2024-09-23 19:40:10 +0200

    Factory et tests

--- pkg/comsoc/cs_test.go
+++ pkg/comsoc/cs_test.go
@@ -119,6 +119,73 @@ func TestApprovalSCF(t *testing.T) {
 	}
 }
 
+func TestTieBreakFactory(t *testing.T) {
+	prefs := [][]Alternative{
+		{1, 3, 2},
+		{2, 1, 3},
+		{2, 1, 3},
+	}
+	thresholds := []int{2, 1, 2}
+
+	res, err := ApprovalSCF(prefs, thresholds)
+	if err != nil {
+		t.Error(err)
+	}
+	tieBreak := TieBreakFactory(prefs[0])
+	result, err := tieBreak(res)
+	if err != nil {
+		t.Error(err)
+	}
+	if result != 1 {
+		t.Errorf("Erreur, le gagnat devrait être 1")
+	}
+}
+
+func TestSCFFactory(t *testing.T) {
+	prefs := [][]Alternative{
+		{1, 3, 2},
+		{2, 1, 3},
+		{2, 1, 3},
+	}
+	thresholds := []int{2, 1, 2}
+	appro2 := func(p Profile) (alt []Alternative, err error) {
+		return ApprovalSCF(p, thresholds)
+	}
+	tieBreak := TieBreakFactory(prefs[0])
+	fn := SCFFactory(appro2, tieBreak)
+	result, err := fn(prefs)
+	if err != nil {
+		t.Error(err)
+	}
+	if result != 1 {
+		t.Errorf("Erreur, le gagnat devrait être 1")
+	}
+}
+
+func TestSWFFactory(t *testing.T) {
+	prefs := [][]Alternative{
+		{1, 2, 3},
+		{1, 2, 3},
+		{2, 1, 3},
+		{2, 1, 3},
+		{3, 1, 2},
+		{3, 2, 1},
+	}
+
+	trieur := []Alternative{1, 3, 2}
+	tieBreak := TieBreakFactory(trieur)
+	fn := SWFFactory(MajoritySWF, tieBreak)
+	res, err := fn(prefs)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(res) != 3 || res[0] != 1 || res[1] != 3 || res[2] != 2 {
+		t.Errorf("error, 1 should be the only best Alternative")
+	}
+}
+
 /* func TestCondorcetWinner(t *testing.T) {
 	prefs1 := [][]Alternative{
 		{1, 2, 3},
--- pkg/comsoc/tiebreak.go
+++ pkg/comsoc/tiebreak.go
@@ -1,6 +1,9 @@
 package comsoc
 
-import "errors"
+import (
+	"errors"
+	"sort"
+)
 
 func TieBreakFactory(orderedAlts []Alternative) func([]Alternative) (Alternative, error) {
 	return func(alts []Alternative) (winner Alternative, err error) {
@@ -16,9 +19,33 @@ func TieBreakFactory(orderedAlts []Alternative) func([]Alternative) (Alternative
 }
 
 func SWFFactory(swf func(p Profile) (Count, error), tb func([]Alternative) (Alternative, error)) func(Profile) ([]Alternative, error) {
+	return func(p Profile) (alts []Alternative, err error) {
+		c, err := swf(p)
+		if err != nil {
+			return
+		}
+		for elem, _ := range c {
+			alts = append(alts, elem)
+		}
+		sort.Slice(alts, func(i, j int) bool {
+			var array []Alternative
+			array = append(array, alts[i])
+			array = append(array, alts[j])
 
+			winner, _ := tb(array)
+			return winner == array[0]
+		})
+		return
+	}
 }
 
 func SCFFactory(scf func(p Profile) ([]Alternative, error), tb func([]Alternative) (Alternative, error)) func(Profile) (Alternative, error) {
-
+	return func(p Profile) (alt Alternative, err error) {
+		c, err := scf(p)
+		if err != nil {
+			return
+		}
+		alt, err = tb(c)
+		return
+	}
 }
